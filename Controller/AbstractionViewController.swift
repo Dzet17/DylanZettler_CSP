b4b4da747aacd58fe9fd2a4e501070f25117a864
Dylan Zettler <dylanzettler@gmail.com>
2017-11-27 08:41:07 -0700

    Fixed code, no errors, pageViewController works fine now

--- Controller/AbstractionViewController.swift
+++ Controller/AbstractionViewController.swift
@@ -8,6 +8,88 @@
 
 import UIKit
 
-class AbstractionViewController: NSObject {
+class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+{
+    //MARK: Array of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper method to retrieve the correc ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
 
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+            }
+        }
+
+    //MARK:- Required Protocol methods for UIPAgeViewControllerDatasource
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+    
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+       
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
+
+    //MARK:- Support for dots in the UIPageViewController
+
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first
 }
